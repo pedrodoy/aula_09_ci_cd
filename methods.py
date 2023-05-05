96a2f70116bc40619aaa87c5c47c03b689f68fae
pedrodoy <pedrodoy01@gmail.com>
2023-05-05 09:17:38 -0300

    Tarefa_aula09

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sum(value1, value2):
+    soma = value1 + value2
+    return soma
+
+def mult(value1, value2):
+    multiplicacao = value1 * value2
+    return multiplicacao
+
+def div(value1, value2):
+    divisao = value1 / value2
+    return divisao
+
+def sub(value1, value2):
+    subtracao  = value1 - value2
+    return subtracao
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,35 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum():
+    value1 = 4
+    value2 = 7
+
+    output = methods.soma(value1, value2)
+
+    assert output == 10
+
+def test_mult():
+    value1 = 1
+    value2 = 8
+
+    output = methods.multiplicacao(value1, value2)
+
+    assert output == 15
+
+def test_div():
+    value1 = 9
+    value2 = 3
+
+    output = methods.divisao(value1, value2)
+
+    assert output == 2
+
+def test_sub():
+    value1 = 4
+    value2 = 7
+
+    output = methods.subtracao(value1, value2)
+
+    assert output == 2
